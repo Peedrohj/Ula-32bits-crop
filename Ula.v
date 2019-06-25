bc93b9264d33958f7c91813c567eaf38592d2e6c
ArthurReis <aor@cesar.school>
2019-06-25 13:04:47 -0300

    Update

--- Ula.v
+++ Ula.v
@@ -1,47 +1,58 @@
-module alu(input1, input2, aluControlOut, shamt, result);
+module Ula(input1, input2, aluControlOut, shamt, result, opCode, clk, isOverflowed, opOverflowed);
 	input signed [31:0] input1, input2;
 	input [2:0] aluControlOut;
 	input [4:0] shamt;
-	output signed [31:0] result;
+	input opCode;
+	input clk;
+	output reg signed [31:0] result;
+	output reg [1:0] isOverflowed;
+	output reg [1:0] opOverflowed;
 	
 	// OverflowCheck
-	if(input1 + input2 > 2147483647) 
-		// foi overflow
-		assign isOverflowed = 1;
-		assign opOverflowed = opCode;
-	else
-		begin
-			assign result = aluControlOut == 3'b000' ? input1 + input2: //ADD/ADDi
-								 aluControlOut == 3'b001' ? input1 - input2: //SUB
-								 aluControlOut == 3'b010' ? input1 & input2: //AND/ANDi
-								 aluControlOut == 3'b011' ? input1 | input2: //OR
-								 aluControlOut == 3'b100' ? input1 << shamt: //SLL
-								 aluControlOut == 3'b101' ? input1 >> shamt: //SRL
-								 aluControlOut == 3'b110' ? input1 >>> shamt: //SRA
-								 aluControlOut == 3'b111' ? input1 < input2 ? 1 : 0; //SLT
-		end		
+	always @ (posedge clk) begin
+		if(input1 + input2 > 2147483647) begin
+			// foi overflow	
+			assign isOverflowed = 01;
+			assign opOverflowed = opCode;
+		end else begin 
+				assign result = aluControlOut == 0 ? input1 + input2: //ADD/ADDi
+									 aluControlOut == 1 ? input1 - input2: //SUB
+									 aluControlOut == 2 ? input1 & input2: //AND/ANDi
+									 aluControlOut == 3 ? input1 | input2: //OR
+									 aluControlOut == 4 ? input1 << shamt: //SLL
+									 aluControlOut == 5 ? input1 >> shamt: //SRL
+									 aluControlOut == 6 ? input1 >>> shamt: //SRA
+									 aluControlOut == 7 ? input1 < input2 ? 1 : 0 : 0; //SLT
+		end
+	end
 endmodule
 
-module aluControl(aluOp, funct, opCode):
-	input [5:0] funct,opCode,opOverflowed;
+module aluControl(aluOp, funct, opCode, opOverflowed, aluControlOut, clk);
+	input [5:0] funct;
+	input [5:0] opCode;
+	input [5:0] opOverflowed;
 	input [1:0] aluOp;
-	assign [2:0] aluControlOut;
+	output clk;
+	output reg [2:0] aluControlOut;
 	assign isOverflowed = 0;
-	
-	if(aluOp == 0)
-		assign aluControlOut = 0; // Chama um add pq era addi
-	if(aluOp == 1)
-		assign aluControlOut = 2; // chama um and pq era andi
-	if(aluOp == 2)
-		funct == 0 ? assign aluControlOut = 3'b100': // SLL
-		funct == 2 ? assign aluControlOut = 3'b101':	// SRL
-		funct == 3 ? assign aluControlOut = 3'b110': // SRA
-		funct == 32 ? assign aluControlOut = 3'b000': // ADD
-		funct == 34 ? assign aluControlOut = 3'b001': // SUB
-		funct == 36 ? assign aluControlOut = 3'b010': // AND | ANDI
-		funct == 37 ? assign aluControlOut = 3'b011': // OR
-		funct == 42 ? assign aluControlOut = 3'b111': // SLT 
-		
 
+	
+	always @ (posedge clk) begin
+		if(aluOp == 0) begin
+			assign aluControlOut = 0; // Chama um add pq era addi
+		end if(aluOp == 1) begin
+			assign aluControlOut = 2; // chama um and pq era andi
+		end if(aluOp == 2) begin
+			
+			assign aluControlOut = funct == 0 ? 4: // SLL
+			funct == 2 ? 5:	// SRL
+			funct == 3 ? 6: // SRA
+			funct == 32 ? 0: // ADD
+			funct == 34 ? 1: // SUB
+			funct == 36 ? 2: // AND | ANDI
+			funct == 37 ? 3: // OR
+			funct == 42 ? 7: 0; // SLT 
+		end
+	end
 endmodule
 						 
